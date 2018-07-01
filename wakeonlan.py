39883a710a118722bec4072dc72dc410f0a0163f
Ally Smith <me@alasdairsmith.com>
2018-07-02 00:59:30 +0100

    Rewritten as a single function.
    A class was not necessary.

--- wakeonlan.py
+++ wakeonlan.py
@@ -1,29 +1,38 @@
+"""Send Wake-On-LAN magic packets using UDP."""
 import socket
 import codecs
 
-class WakeOnLAN:
-    """Send Wake On LAN magic packets."""
+def wakeonlan(mac_address, broadcast_address='255.255.255.255', port=9):
+    """Send a Wake-On-LAN magic packet using UDP.
+    
+    :param str mac_address: MAC address of the device to be wakened.
+    :param str broadcast_address: Broadcast address to be used (default
+    =255.255.255.255).
+    :param int port: UDP port to be used (default=9).
+    :return: If the packet was sent; there is no acknowledgement upon 
+    receipt.
+    :rtype: bool
+    """
 
-    def __init__(self):
-        self.broadcast_address = '255.255.255.255'
-        self.port = 9
-        # Sequence of bytes which indicates a WOL magic packet.
-        self.SYNC_STREAM  = b'\xff\xff\xff\xff\xff\xff'
+    # Sequence of six bytes indicating start of a WOL magic packet.
+    SYNC_STREAM  = b'\xff\xff\xff\xff\xff\xff'
 
-    def send(self, mac_address):
-        """Send a magic wake packet to the target MAC address."""
-        payload = self.SYNC_STREAM + codecs.decode(mac_address * 16, 'hex')
+    if mac_address is None:
+        raise TypeError('MAC address must be provided.')
 
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # Translation table to remove common separator characters
+    table = str.maketrans(dict.fromkeys(':-. '))
+    mac_address = mac_address.translate(table)
+            
+    if len(mac_address) != 12:
+        raise ValueError('MAC address is the wrong length.')
+
+    # The datagram must contain the six byte sequence followed by the
+    # MAC address repeated 16 times.
+    data = SYNC_STREAM + codecs.decode(mac_address * 16, 'hex')
+
+    # Broadcast the datagram
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        sock.sendto(payload, (self.broadcast_address, self.port))
-        sock.close()
-
-class SleepOnLAN(WakeOnLAN):
-    def __init__(self):
-        super().__init__()
-        self.SYNC_STREAM = b'\x7f\x7f\x7f\x7f\x7f\x7f'
-
-if __name__ == "__main__":
-    wol = WakeOnLAN()
-    wol.send('e0d55e204bb3')
+        sock.sendto(data, (broadcast_address, port))
+        return True
